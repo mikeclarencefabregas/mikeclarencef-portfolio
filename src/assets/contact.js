c0ebf42c36943791bd409c027c77591dc2b0e323
Mike Clarence Fabregas <20-31529@g.batstate-u.edu.ph>
2024-09-01 22:57:18 +0800

    new changes

--- src/assets/contact.js
+++ src/assets/contact.js
@@ -1,5 +1,5 @@
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Lipa City, Batangas Philippines",
+  phoneNo: "0955-405-9911",
+  email: "mikeclarencefabregas@gmail.com",
 };
