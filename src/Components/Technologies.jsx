0e9ac227cc7a369a7c2a67a0025238c9a417a4c9
Mike Clarence Fabregas <20-31529@g.batstate-u.edu.ph>
2024-09-01 22:44:31 +0800

    new changes

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -20,10 +20,22 @@ const iconVariants = (duration) => ({
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h1 className="my-20 text-center text-4xl">Technologies</h1>
-      <div className="flex flex-wrap items-center justify-center gap-4">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
         <motion.div
-          variants={iconVariants(2.5)}
+          variants={iconVariants(3)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -39,7 +51,7 @@ const Technologies = () => {
           <FaPython className="text-7xl" />
         </motion.div>
         <motion.div
-          variants={iconVariants(2.5)}
+          variants={iconVariants(4)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -47,7 +59,7 @@ const Technologies = () => {
           <FaHtml5 className="text-7xl text-orange-700" />
         </motion.div>
         <motion.div
-          variants={iconVariants(2.5)}
+          variants={iconVariants(6)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
@@ -55,14 +67,14 @@ const Technologies = () => {
           <FaCss3 className="text-7xl text-blue-900" />
         </motion.div>
         <motion.div
-          variants={iconVariants(2.5)}
+          variants={iconVariants(5)}
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <FaJsSquare className="text-7xl text-yellow-400" />
         </motion.div>
-      </div>
+      </motion.div>
     </div>
   );
 };
