d3191a3aa8bf0db0fb378e326551839ab9044f1a
Mike Clarence Fabregas <20-31529@g.batstate-u.edu.ph>
2024-09-01 18:48:34 +0800

    new changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,6 +4,7 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -16,6 +17,7 @@ const Navbar = () => {
         <FaInstagram />
         <FaLinkedin />
         <FaSquareTwitter />
+        <FaFacebook />
       </div>
     </nav>
   );
